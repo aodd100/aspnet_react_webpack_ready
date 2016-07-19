be9b486453e8c501e79c26968a6131b71948bcb0
aodd100 <oddiri2013@gmail.com>
2016-07-19 11:46:55 -0700

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BisterGram.Startup))]
+namespace BisterGram
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
